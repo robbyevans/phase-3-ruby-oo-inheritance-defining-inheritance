adb31b6d9934de4cb30f7f4d37edcf3a675536da
Evans Rutto <robbyevans001@gmail.com>
2022-08-25 21:49:37 +0300

    commit tree d9db8d6b5208996fc358632ce1ff6116badaa682

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,14 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+class Car < Vehicle
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
+
+car=Car.new(16,4)
+
+pp car.wheel_number
+pp car.wheel_size
+pp car.go
+pp car.fill_up_tank
